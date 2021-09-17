8384eaaea7671a5de515cf5db364406b618d7547
Gavin <7797864896@K12.LCS>
2021-09-17 10:39:20 -0400

    Add panel route file and connect Admin Feature form to panelsDB

--- client/src/Components/AdminFeature.jsx
+++ client/src/Components/AdminFeature.jsx
@@ -4,68 +4,67 @@ import Header from './Header.jsx';
 
 function AdminFeature(){
 
-    const [userFeature, setUserFeature] = useState({
-        userPic: "",
-        userHeading: ""
-    });
+    const [feature, setFeature] = useState([{
+        userPhoto: '',
+        userTitle: '',
+    }])
 
-    function handlePicChange(event){
-        setUserFeature({
-            userHeading: event.target.value
-        });
-    }
+    function handleChange(event){
+        const {name, value} = event.target;
 
-    function handleHeadingChange(event){
-        setUserFeature({
-            userPic: event.target.value
-        });
+        setFeature(prevFeatures => {
+            return {
+                ...prevFeatures,
+                [name]: value
+            }
+        })
     }
 
     function handleSubmit(event){
         event.preventDefault();
-        axios.post({
-            method: 'post',
-            url: "http://localhost:5000/add",
-            data: {
-                photo: userFeature.userPic
-            }
-        })
-            .then(function(res){
-                console.log(res.data)
-            })
-    }
 
+        console.log(feature);
+
+        const newPanel = {
+            photo: feature.userPhoto,
+            title: feature.userTitle
+        }
+
+        console.log(newPanel);
+        
+        axios.post("http://localhost:5000/create-panel", newPanel);
+    }
     
     return (
         <div>
             <Header />
             <section class="admin-feature-section">
             <h1 class="section-title">Administrator Feature Assignment</h1>
-            <form class="form-signin" method="post" enctype="application/x-www-form-urlencoded" onSubmit={handleSubmit}>
+            <form class="form-signin" method="post" onSubmit={handleSubmit} enctype="application/x-www-form-urlencoded">
                 <div class="form-group row">
                     <label class="feature-label col-sm-2 col-form-label-lg">Photo:</label>
                     <input 
-                        onChange={handlePicChange}
+                        onChange={handleChange}
                         class="form-control col-sm-10" 
-                        value={userFeature.userPic}
+                        value={feature.photo}
                         name="userPhoto"
                         type="text" 
                         placeholder="Paste photo url here">
                     </input>
                 </div>
-                {/* <div class="form-group row">
+                <div class="form-group row">
                     <label class="feature-label col-sm-2 col-form-label-lg">Feature Title: </label>
                     <input 
-                        onChange={handleHeadingChange}
+                        onChange={handleChange}
                         class="form-control col-sm-10" 
-                        value={userFeature.userHeading} 
+                        value={feature.title} 
                         name="userTitle"  
                         type="text" 
                         placeholder="Type the title of the feature here" 
                     />
-                </div> */}
+                </div>
                 
-                <button class="btn btn-lg submit-btn">Submit</button>
+                <button class="btn btn-lg submit-btn" type="submit">Submit</button>
             </form>
             </section>
         </div>
--- routes/panelRoute.js
+++ routes/panelRoute.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Panel = require('../models/panelModel.js');
 
-router.route("/create").post((req, res) => {
+router.route("/create-panel").post((req, res) => {
+    const photoUrl = req.body.photo;
+    const title = req.body.title;
+    
+    console.log(photoUrl);
+    
+    const newPanel = new Panel({
+        photoUrl,
+        title
+    });
 
+    newPanel.save();
 });
+
+module.exports = router;
