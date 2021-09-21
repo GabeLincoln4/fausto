4778b39b9a00adb9f48d050450dd28af4dc7073e
Gavin <7797864896@K12.LCS>
2021-09-21 16:22:48 -0400

    add tailwind css and begin redesigning landing page

--- client/src/Components/Feature.jsx
+++ client/src/Components/Feature.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
 import SeasonalItem from "./SeasonalItem.jsx";
-
+import CoffeeHangOut from "../images/hang-out.svg"
 
 function Feature(){
-    return <h1>Test</h1>
+    return (<div className="grid grid-cols-2 container mx-auto py-5">
+        <h1>Test</h1>
+        <img className="w-full h-auto" src={CoffeeHangOut} alt="coffee-hang-out" />
+    </div>)
 }
 
 export default Feature;
