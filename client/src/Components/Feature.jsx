dbf15f6f5c8e48018bc486c9d344853eef090b98
Gavin <7797864896@K12.LCS>
2021-09-21 18:06:33 -0400

    create heading and put landing page elements in grid

--- client/src/Components/Feature.jsx
+++ client/src/Components/Feature.jsx
@@ -3,9 +3,11 @@ import SeasonalItem from "./SeasonalItem.jsx";
 import CoffeeHangOut from "../images/hang-out.svg"
 
 function Feature(){
-    return (<div className="grid grid-cols-2 container mx-auto py-5">
-        <h1>Test</h1>
-        <img className="w-full h-auto" src={CoffeeHangOut} alt="coffee-hang-out" />
+    return (<div className="relative grid md:grid-cols-2 grid-cols-1 container mx-auto py-5">
+        <div className="flex">
+        <h1 className="text-center md:text-left text-5xl md:text-7xl">Freshly Roasted. Downtown Feel.</h1>
+        </div>
+        <img className="w-full h-auto mt-3 md:flex-shrink-0" src={CoffeeHangOut} alt="coffee-hang-out" />
     </div>)
 }
 
