ff50a0477313faa79184162ea1292930cc242b2c
GabeLincoln4 <ggavin@outlook.com>
2022-03-06 18:42:57 -0500

    remove gitignore

--- client/src/Components/Feature.jsx
+++ client/src/Components/Feature.jsx
@@ -35,7 +35,6 @@ function Feature(){
 
     return (
         <div className="bg-yellow-100 h-full" id="home">
-            <h1 className="text-red-400">Test</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 pt-20" data-aos="zoom-in">
                         <div className="md:p-4 text-center md:self-end h-2/3">
                             <h1 className="md:text-left text-6xl md:text-7xl leading-tight">Freshly <span className="text-yellow-800" whileHover="text-white">Roasted. </span>Downtown <span className="text-yellow-400">Feel.</span></h1>
