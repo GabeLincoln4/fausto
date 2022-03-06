0961493603839f70254664f087e8fa6260a4bb51
GabeLincoln4 <ggavin@outlook.com>
2022-03-06 16:58:23 -0500

    Add gitignore, Procfile and update ports

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ app.use(cors());
 app.use(express.json());
 
 //connect to mongoose
-mongoose.connect("mongodb+srv://admin-gabriel:<password>@cluster0.n51sy.mongodb.net/panelsDB");
+mongoose.connect("mongodb+srv://admin-gabriel:xiqW75HDDMmoRhiK@cluster0.n51sy.mongodb.net/panelsDB");
 
 //require route
 app.use("/", require("./routes/featuresRoute.js"));
 app.use("/", require("./routes/panelRoute.js"));
 app.use("/", require("./routes/menuItemRoute.js"));
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
 
-
-app.listen(5000, function(){
-    console.log("Server is running at port 5000");
+app.listen(port, function(){
+    console.log("Server has started successfully");
 });
