7e5a168bbfb2fda8006338906b9fe8bdcaa08a9f
Gavin <7797864896@K12.LCS>
2022-03-07 14:40:33 -0500

    add procfile

--- client/app.js
+++ client/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const path = require('path');
+// const path = require('path');
 
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static('build'));
-}
+// if (process.env.NODE_ENV === "production"){
+//     app.use(express.static('build'));
+// }
 
-app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log('Server running on port: ', port);
-});
+// app.listen(port, (err) => {
+//     if (err) return console.log(err);
+//     console.log('Server running on port: ', port);
+// });
 
